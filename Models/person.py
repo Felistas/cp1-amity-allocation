d015a720280b72a7946ed7ead97b9c34930cdd01
Felistas Ngumi <Shera@Felistass-MacBook-Pro.local>
2017-03-23 11:49:33 +0300

    Define Person,Fellow and Staff class

---
+++ Models/person.py
@@ -0,0 +1,11 @@
+from app import Amity
+class Person(Amity):
+    def __init__(self):
+        self.all_person = ["john"]
+    def add_person(self,name,role,accommodation):
+        pass
+
+class Fellow(Person):
+    pass
+class Staff(Person):
+    pass
