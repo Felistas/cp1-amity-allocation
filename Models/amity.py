5443e191f70c899066d373d9f579916aeb936b94
Felistas Ngumi <Shera@Felistass-MacBook-Pro.local>
2017-03-29 15:56:35 +0300

    Add amity file with all functions

---
+++ Models/amity.py
@@ -0,0 +1,19 @@
+class Amity:
+    def create_room(self,name,type):
+        return 'Created room successfully'
+    def add_person(self,name,role,accommodation):
+        return 'Added person successfully'
+    def reallocate_person(self):
+        pass
+    def load_people(self):
+        pass
+    def print_allocation(self):
+        pass
+    def print_unallocated(self):
+        pass
+    def print_room(self):
+        pass
+    def save_state(self):
+        pass
+    def load_state(self):
+        pass
