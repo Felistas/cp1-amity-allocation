a297ca20e769959aa4104d23124c4c91e78d4769
Felistas <felistas.ngumi@andela.com>
2017-04-06 18:53:04 +0300

    Add docopt file and ammend create room function to take multiple rooms

--- Models/amity.py
+++ Models/amity.py
@@ -6,19 +6,25 @@ class Amity:
     office_waitiing_list = []
     living_space_waitiing_list = []
 
-    def create_room(self,room_name,room_type):
-        room_name == room_name.upper()
-        room_type == room_type.upper()
-        rooms = self.rooms['living_space'] + self.rooms['office']
-        check_room = [room.room_name for room in rooms if room.room_name == room_name]
-        if check_room:
-            return 'The room already exists'
-        if room_type == 'office':
-            room = Office(room_name)
-            Amity.rooms['office'].append(room)
+    def create_room(self,room_type,room_names):
+        room_type = room_type.upper()
+        if room_type in ['OFFICE','LIVINGSPACE']:
+            for room_name in room_names:
+                rooms = self.rooms['living_space'] + self.rooms['office']
+                check_room = [room.room_name for room in rooms if room.room_name == room_name]
+                if check_room:
+                    print ('The room already exists')
+                else:
+                    if room_type == 'OFFICE':
+                        room = Office(room_name)
+                        Amity.rooms['office'].append(room)
+                        print ('{} successfully created'.format(room_name))
+                    elif room_type == 'LIVINGSPACE':
+                        room = LivingSpace(room_name)
+                        Amity.rooms['living_space'].append(room)
+                        print ('{} successfully created'.format(room_name))
         else:
-            room = LivingSpace(room_name)
-            Amity.rooms['living_space'].append(room)
+            print ('Invalid room type')
 
     def add_person(self, first_name, last_name, accommodation):
         pass
@@ -38,7 +44,7 @@ class Amity:
         pass
 
 
-amity = Amity()
-amity.create_room('tsavo',"office")
-amity.create_room('tsao',"office")
-print(amity.rooms)
+# amity = Amity()
+# amity.create_room('tsavo',"office")
+# amity.create_room('tsao',"office")
+# print(amity.rooms)
--- Models/app.py
+++ Models/app.py
@@ -1,24 +1,86 @@
-"""Naval Fate.
+#!/usr/bin/env python
+"""
+This example uses docopt with the built in cmd module to demonstrate an
+interactive command application.
 
 Usage:
-  naval_fate.py ship new <name>...
-  naval_fate.py ship <name> move <x> <y> [--speed=<kn>]
-  naval_fate.py ship shoot <x> <y>
-  naval_fate.py mine (set|remove) <x> <y> [--moored | --drifting]
-  naval_fate.py (-h | --help)
-  naval_fate.py --version
-
+    Amity create_room <room_name> <room_type>
+    Amity add_person <person_name> <FELLOW|STAFF> [wants_accommodation]
+    Amity reallocate_person <person_identifier> <new_room_name>
+    Amity load_people
+    Amity print_allocations [-o=filename]
+    Amity print_unallocated [-o=filename]
+    Amity print_room <room_name>
+    Amity save_state [--db=sqlite_database]
+    Amity load_state <sqlite_database>
+    Amity (-i | --interactive)
+    Amity (-h | --help | --version)
 Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  --speed=<kn>  Speed in knots [default: 10].
-  --moored      Moored (anchored) mine.
-  --drifting    Drifting mine.
-
+    -i, --interactive  Interactive Mode
+    -h, --help  Show this screen and exit.
 """
-from docopt import docopt
+
+import sys
+import cmd
+from docopt import docopt, DocoptExit
+from amity import Amity
 
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version='Naval Fate 2.0')
-    print(arguments)
+def docopt_cmd(func):
+
+    def fn(self, arg):
+        try:
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+
+            print('Invalid Command!')
+            print(e)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
+
+
+class Amityapp (cmd.Cmd):
+    intro = 'Welcome to Amity room allocations!' \
+        + ' (type help for a list of commands.)'
+    prompt = 'Enter command>> '
+    file = None
+    amity = Amity()
+
+    @docopt_cmd
+    def do_create_room(self, arg):
+        """Usage: create_room <room_type> <room_name>..."""
+        self.amity.create_room(arg['<room_type>'],arg['<room_name>'])
+
+    @docopt_cmd
+    def do_add_person(self, arg):
+        """Usage: add_person <person_name> <FELLOW|STAFF> [wants_accommodation]"""
+
+        print(arg)
+
+    def do_quit(self, arg):
+        """Quits out of Interactive Mode."""
+
+        print('Good Bye!')
+        exit()
+
+opt = docopt(__doc__, sys.argv[1:])
+
+if opt['--interactive']:
+    Amityapp().cmdloop()
+
+print(opt)
